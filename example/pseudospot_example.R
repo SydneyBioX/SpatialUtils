bb7979dd08092edbec12ef3b2a22864321d5e36c
PratibhaPanwar <pratibhapanwar.4@gmail.com>
2024-02-01 11:34:32 +1100

    Added code to show how to generate spe object from me object using pseudospot boundaries. Corrected readVisium to readXenium when reading repoDir (Xenium example) into me object.

--- example/pseudospot_example.R
+++ example/pseudospot_example.R
@@ -6,7 +6,7 @@ library(ggforce)
 
 repoDir <- system.file("extdata", package = "MoleculeExperiment")
 repoDir <- paste0(repoDir, "/xenium_V1_FF_Mouse_Brain")
-me <- MoleculeExperiment::readVisium(repoDir,
+me <- MoleculeExperiment::readXenium(repoDir,
                                       keepCols = "essential",
                                       addBoundaries = "cell")
 
@@ -19,8 +19,14 @@ me <- pseudospotBoundaries(me,
                            start_margin = -55, 
                            size_of_spot = 55)
 
-ggplot_me() +
+spot_overlay <- ggplot_me() +
     # add cell segments and colour by cell id
     geom_polygon_me(me, byFill = "segment_id", colour = "black", alpha = 0.1) +
     # add nuclei segments and colour the border with red
     geom_polygon_me(me, assayName = "pseudospot", fill = NA, colour = "red", size = 0.2) 
+spot_overlay
+
+# create a spatialExperiment object using pseudospots as boundaries
+# this aggregates the gene expression within each spot
+# the spatialCoords of spe now point to the centres of the pseudospots  
+spe <- countMolecules(me, boundariesAssay = "pseudospot")
